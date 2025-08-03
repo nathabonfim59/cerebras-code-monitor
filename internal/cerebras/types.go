00f79684d41aebf05c149378e6e0b4cc3787131c
nathabonfim59 <dev@nathabonfim59.com>
2025-08-03 02:02:12 -0300

    feat(metrics): update types based on graphql responses

--- internal/cerebras/types.go
+++ internal/cerebras/types.go
@@ -7,6 +7,22 @@ type Quota struct {
 	ResetTime string `json:"reset_time"`
 }
 
+// UsageQuota represents usage quota information for an organization
+type UsageQuota struct {
+	ModelId             string `json:"modelId"`
+	RegionId            string `json:"regionId"`
+	OrganizationId      string `json:"organizationId"`
+	RequestsPerMinute   string `json:"requestsPerMinute"`
+	TokensPerMinute     string `json:"tokensPerMinute"`
+	RequestsPerHour     string `json:"requestsPerHour"`
+	TokensPerHour       string `json:"tokensPerHour"`
+	RequestsPerDay      string `json:"requestsPerDay"`
+	TokensPerDay        string `json:"tokensPerDay"`
+	MaxSequenceLength   string `json:"maxSequenceLength"`
+	MaxCompletionTokens string `json:"maxCompletionTokens"`
+	Typename            string `json:"__typename"`
+}
+
 // UsageMetrics represents the usage metrics for an organization
 type UsageMetrics struct {
 	OrganizationID string  `json:"organization_id"`
@@ -18,10 +34,26 @@ type UsageMetrics struct {
 	Quotas         []Quota `json:"quotas"`
 }
 
+// OrganizationUsage represents usage data for an organization
+type OrganizationUsage struct {
+	ModelId  string `json:"modelId"`
+	RegionId string `json:"regionId"`
+	RPM      string `json:"rpm"`
+	TPM      string `json:"tpm"`
+	RPH      string `json:"rph"`
+	TPH      string `json:"tph"`
+	RPD      string `json:"rpd"`
+	TPD      string `json:"tpd"`
+	Typename string `json:"__typename"`
+}
+
 // Organization represents a Cerebras organization
 type Organization struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID               string `json:"id"`
+	Name             string `json:"name"`
+	OrganizationType string `json:"organizationType"`
+	State            string `json:"state"`
+	Typename         string `json:"__typename"`
 }
 
 // OrganizationsResponse represents the response from the organizations endpoint
